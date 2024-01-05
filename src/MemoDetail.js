12475bd9234d77bf82e2d58d17977705ad33c24e
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-01-05 17:35:57 +0900

    詳細画面でメモの内容を表示できるようにする

--- src/MemoDetail.js
+++ src/MemoDetail.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MemoDetail({ memo, onEditClick, onDeleteClick }) {
+  const [text, setText] = useState(memo.title + '\n' + memo.content);
+
   return (
     <div>
       <h2>詳細</h2>
-      <textarea value={memo.title + '\n' + memo.content}>
+      <textarea
+        value={text}
+        onChange={e => setText(e.target.value)}
+      >
       </textarea>
       <p></p>
       <button onClick={onEditClick}>編集</button>
